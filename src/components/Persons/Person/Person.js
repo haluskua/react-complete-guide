d77f4639dffa50598443b4618e2888e54f4c027c
Halus <haluskua.one@gmail.com>
2019-11-01 07:53:07 +1300

    component lifecycle action in creation

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -18,7 +18,7 @@ import React from 'react';
 import classes from './Person.css'
 
 const person = ( props ) => {
- 
+    console.log('[Person.js] rendering ...')
     return (
     <div className={classes.Person}>
         <p onClick={props.click}>I am {props.name} and I am {props.age} years olds! </p>
