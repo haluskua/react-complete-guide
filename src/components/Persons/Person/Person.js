daf6c53f9db680d097948e3663b8900a857db284
Haluskua <haluskua.one@gmail.com>
2019-12-19 16:52:32 +1300

    accessing jsx with ceateRef() with constructor and super and ref ={this.input const pointing to reacRef()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -21,49 +21,65 @@ import withClass from "../../../hoc/WithClass";
 import PropTypes from "prop-types";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+    // not related to React App so not the optimal way so use id
+    // document.querySelector("input").focus();
+    // this.inputElement.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering ...");
-    // return [
-    //   <p key="i1" onClick={this.rops.click}>
-    //     I am {this.props.name} and I am {this.props.age} years olds!{" "}
-    //   </p>,
-    //   <p key="i2">{this.props.children}</p>,
-    //   <input
-    //     key="i3"
-    //     type="text"]
-    //     onChange={this.props.changed}
-    //     value={this.props.name}
-    //   />
-    // ];
-    // return (
-    //   <Auxiliary>
-    //     <p key="i1" onClick={this.props.click}>
-    //       I am {this.props.name} and I am {this.props.age} years olds!{" "}
-    //     </p>
-    //     <p key="i2">{this.props.children}</p>
-    //     <input
-    //       key="i3"
-    //       type="text"
-    //       onChange={this.props.changed}
-    //       value={this.props.name}
-    //     />
-    //   </Auxiliary>
-    // );
+
     return (
-      <React.Fragment>
+      <Auxiliary>
         <p key="i1" onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years olds!{" "}
         </p>
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </React.Fragment>
+      </Auxiliary>
     );
   }
+  // return [
+  //   <p key="i1" onClick={this.rops.click}>
+  //     I am {this.props.name} and I am {this.props.age} years olds!{" "}
+  //   </p>,
+  //   <p key="i2">{this.props.children}</p>,
+  //   <input
+  //     key="i3"
+  //     type="text"]
+  //     onChange={this.props.changed}
+  //     value={this.props.name}
+  //   />
+  // ];
+  // return (
+  //   <Auxiliary>
+  //     <p key="i1" onClick={this.props.click}>
+  //       I am {this.props.name} and I am {this.props.age} years olds!{" "}
+  //     </p>
+  //     <p key="i2">{this.props.children}</p>
+  //     <input
+  //       key="i3"
+  //       type="text"
+  //       onChange={this.props.changed}
+  //       value={this.props.name}
+  //     />
+  //   </Auxiliary>
+  // );
 }
 //when distribution to other developers or when working in large group or projects
 Person.proptTypes = {
