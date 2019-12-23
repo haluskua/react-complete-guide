0a511457edd890b7f21ca0cd8b5139c54ffdaf5e
Haluskua <haluskua.one@gmail.com>
2019-12-23 13:12:47 +1300

    LEGACY writing a stateful component with a HOC that returns a function which pass on classNames

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 // const withClass = props => (
 //   <div className={props.classes}>{props.children}</div>
@@ -6,12 +6,29 @@ import React from "react";
 
 // the otherway of using HOC
 
+// const withClass = (WrappedComponent, className) => {
+//   return (props) => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   );
+// };
+
+
+// Does not have to be a functional compoenent
+// When fetching API to confirm user ID etc you will use a stateful Component
+
 const withClass = (WrappedComponent, className) => {
-  return (props) => (
-    <div className={className}>
-      <WrappedComponent {...props} />
-    </div>
-  );
+  return class extends Component {
+    render () {
+      return (
+        <div className={className}>
+        <WrappedComponent {...this.props} />
+      </div>
+      )
+    }
+
+  }
 };
 
 export default withClass;
